7fd09edb7f81a86a85960b0b88d586e5531157d8
冰剑 <icesword28@qq.com>
2013-05-24 18:04:00 +0800

    用户绑定
    用户绑定

--- Action.php
+++ Action.php
@@ -2,15 +2,15 @@
 /**
  * WeChatHelper Plugin
  *
- * @copyright  Copyright (c) 2011 DEFE (http://defe.me)
+ * @copyright  Copyright (c) 2013 Binjoo (http://binjoo.net)
  * @license    GNU General Public License 2.0
  * 
  */
 
 class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
 {
-    private $db;
-
+    private $_db;
+    private $_options;
 
     private $_textTpl;
     private $_imageTpl;
@@ -20,7 +20,8 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
     {
         parent::__construct($request, $response, $params);
 
-        $this->db = Typecho_Db::get();    
+        $this->_db = Typecho_Db::get();
+        $this->_options = Helper::options()->plugin('WeChatHelper');
         $this->_textTpl = "<xml>
                             <ToUserName><![CDATA[%s]]></ToUserName>
                             <FromUserName><![CDATA[%s]]></FromUserName>
@@ -41,7 +42,7 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
         $this->_itemTpl = "<item>
                              <Title><![CDATA[%s]]></Title> 
                              <Description><![CDATA[%s]]></Description>
-                             <PicUrl><![CDATA[http://s0.binjoo.net/201304/494/35317_z.png]]></PicUrl>
+                             <PicUrl><![CDATA[%s]]></PicUrl>
                              <Url><![CDATA[%s]]></Url>
                              </item>";
     }
@@ -53,7 +54,7 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
     public function link()
     {
         if($this->request->isGet()){
-            $this->getAction();
+            //$this->getAction();
         }
         if($this->request->isPost()){
             $this->postAction();
@@ -82,49 +83,59 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
         $postStr = file_get_contents("php://input");//$this->request->get("HTTP_RAW_POST_DATA");
         if (!empty($postStr)){
                 $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $time = time();
                 $msgType = $postObj->MsgType;
                 $keyword = trim($postObj->Content);
-                $cmd = substr($keyword,0,1);
+                $cmd = strtolower(substr($keyword, 0, 1));
+                $resultStr = "";
                 if($msgType == "text"){
                     if($cmd=="h"){
-                        $contentStr = "\"n\" 最新日志\n\"r\" 随机日志\n\"f\" 访客评论排行榜\n\"s 关键词\" 搜索日志";
-                        $resultStr = sprintf($this->_textTpl, $fromUsername, $toUsername, $time, $contentStr);
-                    }elseif ($cmd=="f") {
+                        $contentStr = "\"n\" 最新日志\n\"r\" 随机日志\n\"l\" 手气不错\n\"s 关键词\" 搜索日志\n\"f\" 访客评论排行榜";
+                        $resultStr = $this->baseText($postObj, $contentStr);
+                    }elseif ($cmd=="f") {//访客排行
                         $resultStr = $this->commentRank($postObj);
-                    }elseif ($cmd=="r") {
+                    }elseif ($cmd=="r") {//随机日志
                         $resultStr = $this->randomPost($postObj);
-                    }elseif ($cmd=="n") {
+                    }elseif ($cmd=="n") {//最新日志
                         $resultStr = $this->newPost($postObj);
-                    }elseif ($cmd=="s") {
-                        $searchParam = substr($keyword, 1);
+                    }elseif ($cmd=="l") {//手气不错
+                        $resultStr = $this->luckyPost($postObj);
+                    }elseif ($cmd=="s") {//搜索日志
+                        $searchParam = trim(substr($keyword, 1));
                         $resultStr = $this->searchPost($postObj, $searchParam);
+                    }elseif ($cmd=="b" && !$this->isBind()) {//用户绑定
+                        $contentStr = $this->bindUser($postObj);
+                        $resultStr = $this->baseText($postObj, $contentStr);
+                    }elseif ($cmd=="u" && $this->isBindUser($postObj)){//用户解绑命令
+                        $contentStr = $this->unbindUser($postObj);
+                        $resultStr = $this->baseText($postObj, $contentStr);
                     }
                 }else if($msgType == "event"){
-
+                    if($postObj->Event == "subscribe"){
+                        $contentStr = $this->_options->welcome;
+                        $resultStr = $this->baseText($postObj, $contentStr);
+                    }
+                }
+                if($resultStr == ""){
+                    $resultStr = $this->baseText($postObj);
                 }
                 echo $resultStr;
         }else {
             echo "";
             exit;
         }
-/*
-        $option = Typecho_Widget::widget('Widget_Options')->plugin('WeChatHelper');
+        /*
         $dir = __TYPECHO_ROOT_DIR__ . DIRECTORY_SEPARATOR;
-        $myfile = $dir.'nihao.txt';
+        $myfile = $dir.'wechatDebug.txt';
         echo $myfile;
         $file_pointer = @fopen($myfile,"a");
-        @fwrite($file_pointer, $resultStr);
+        @fwrite($file_pointer, $postObj->FromUserName." - ");
+        @fwrite($file_pointer, $postObj->ToUserName." | ");
         @fclose($file_pointer);
-*/
+        */
     }
 
     public function action(){
         $this->widget('Widget_User')->pass('administrator');
-        //$this->on($this->request->is('add'))->add();
-        //$this->on($this->request->is('del'))->del($this->request->del);
         $this->response->goBack();
     }
 
@@ -147,16 +158,26 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
         }
     }
 
+    /** 基础文本信息 **/
+    private function baseText($postObj, $contentStr = null){
+        if($contentStr == null){
+            $contentStr = '不明白你在说什么，但是你可以发送\'h\'来查看帮助！';
+        }
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $time = time();
+        $resultStr = sprintf($this->_textTpl, $fromUsername, $toUsername, $time, $contentStr);
+        return $resultStr;
+    }
+
     /** 最新日志 **/
     private function newPost($postObj){
-        $_imageNum = Helper::options()->plugin('WeChatHelper')->imageNum;
-        $db = Typecho_Db::get();
-        $sql = $db->select()->from('table.contents')
+        $sql = $this->_db->select()->from('table.contents')
             ->where('table.contents.status = ?', 'publish')
             ->where('table.contents.type = ?', 'post')
             ->order('table.contents.created', Typecho_Db::SORT_DESC)
-            ->limit($_imageNum);
-        $result = $db->fetchAll($sql);
+            ->limit($this->_options->imageNum);
+        $result = $this->_db->fetchAll($sql);
 
         $resultStr = $this->sqlData($postObj, $result);
         return $resultStr;
@@ -164,13 +185,25 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
 
     /** 随机日志 **/
     private function randomPost($postObj){
-        $_imageNum = Helper::options()->plugin('WeChatHelper')->imageNum;
-        $db = Typecho_Db::get();
-        $sql = $db->select()->from('table.contents')
-            ->where('table.contents.status = ?','pu2blish')
-            ->limit($_imageNum)
+        $sql = $this->_db->select()->from('table.contents')
+            ->where('table.contents.status = ?','publish')
+            ->limit($this->_options->imageNum)
             ->order('RAND()');
-        $result = $db->fetchAll($sql);
+        $result = $this->_db->fetchAll($sql);
+
+        $resultStr = $this->sqlData($postObj, $result);
+        return $resultStr;
+    }
+
+    /** 手气不错 **/
+    private function luckyPost($postObj){
+        $sql = $this->_db->select()->from('table.contents')
+            ->where('table.contents.status = ?','publish')
+            ->where('table.contents.type = ?', 'post')
+            ->where('table.contents.password IS NULL')
+            ->limit(1)
+            ->order('RAND()');
+        $result = $this->_db->fetchAll($sql);
 
         $resultStr = $this->sqlData($postObj, $result);
         return $resultStr;
@@ -178,32 +211,40 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
 
     /** 搜索日志 **/
     private function searchPost($postObj, $searchParam){
-        $_imageNum = Helper::options()->plugin('WeChatHelper')->imageNum;
         $searchParam = '%' . str_replace(' ', '%', $searchParam) . '%';
 
-        $db = Typecho_Db::get();
-        $sql = $db->select()->from('table.contents')
+        $sql = $this->_db->select()->from('table.contents')
             ->where('table.contents.password IS NULL')
             ->where('table.contents.title LIKE ? OR table.contents.text LIKE ?', $searchParam, $searchParam)
             ->where('table.contents.status = ?', 'publish')
             ->where('table.contents.type = ?', 'post')
             ->order('table.contents.created', Typecho_Db::SORT_DESC)
-            ->limit($_imageNum);
-        $result = $db->fetchAll($sql);
+            ->limit($this->_options->imageNum);
+        $result = $this->_db->fetchAll($sql);
 
         $resultStr = $this->sqlData($postObj, $result);
         return $resultStr;
     }
 
-    private function sqlData($postObj, $data, $imageNum){
-        $_imageNum = Helper::options()->plugin('WeChatHelper')->imageNum;
+    private function sqlData($postObj, $data){
         $_subMaxNum = Helper::options()->plugin('WeChatHelper')->subMaxNum;
         $resultStr = "";
+        $num = 0;
+        $tmpPicUrl = "";
         if($data != null){
             foreach($data as $val){
                 $val = Typecho_Widget::widget('Widget_Abstract_Contents')->filter($val);
-                $content = Typecho_Common::subStr(strip_tags($val['text']), 0, $_subMaxNum, '...');
-                $resultStr .= sprintf($this->_itemTpl, $val['title'], $content, $val['permalink']);
+                $content = Typecho_Common::subStr($this->deleteHtml($val['text']), 0, $_subMaxNum, '...');
+                
+                $preg = "/<img\ssrc=(\'|\")(.*?)\.(jpg|png)(\'|\")/is";
+                preg_match($preg, $val['text'], $images);
+                if($images==null){
+                    $tmpPicUrl = $this->_options->imageDefault;
+                }else{
+                    $tmpPicUrl = $images[2].'.'.$images[3];
+                }
+                $resultStr .= sprintf($this->_itemTpl, $val['title'], $content, $tmpPicUrl, $val['permalink']);
+                $num++;
             }
         }else{
                 $resultStr = "没有找到任何信息！";
@@ -212,7 +253,7 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
         $toUsername = $postObj->ToUserName;
         $time = time();
         if($data != null){
-            $resultStr = sprintf($this->_imageTpl, $fromUsername, $toUsername, $time, $_imageNum, $resultStr);
+            $resultStr = sprintf($this->_imageTpl, $fromUsername, $toUsername, $time, $num, $resultStr);
         }else{
             $resultStr = sprintf($this->_textTpl, $fromUsername, $toUsername, $time, $resultStr);
         }
@@ -222,8 +263,7 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
     /** 水墙 **/
     private function commentRank($postObj){
         $_rankNum = Helper::options()->plugin('WeChatHelper')->rankNum;
-        $db = Typecho_Db::get();
-        $sql = $db->select('COUNT(author) AS cnt','author', 'url', 'mail')
+        $sql = $this->_db->select('COUNT(author) AS cnt','author', 'url', 'mail')
             ->from('table.comments')
             ->where('status = ?', 'approved')
             ->where('type = ?', 'comment')
@@ -232,7 +272,7 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
             ->group('author')
             ->order('cnt', Typecho_Db::SORT_DESC)
             ->limit($_rankNum);
-        $result = $db->fetchAll($sql);
+        $result = $this->_db->fetchAll($sql);
         $contentStr = "";
         $num = 1;
         foreach ($result as $val)
@@ -244,6 +284,78 @@ class WeChatHelper_Action extends Typecho_Widget implements Widget_Interface_Do
         $time = time();
         $resultStr = sprintf($this->_textTpl, $fromUsername, $toUsername, $time, $contentStr);
         return $resultStr;
+    }
+
+    /** 绑定微信用户 **/
+    private function bindUser($postObj){
+        $_bindCaptcha = $this->_options->bindCaptcha;
+        $_bindUserNo = $this->_options->bindUserNo;
+        $resultStr = "";
+        if($_bindUserNo == null || $_bindUserNo == ""){
+            $content = trim(substr(trim($postObj->Content), 1));
+            if($content == $_bindCaptcha){
+                $array["bindUserNo"] = (string) $postObj->FromUserName;
+                Widget_Plugins_Edit::configPlugin("WeChatHelper", $array);
+                $resultStr = "恭喜，绑定成功！";
+            }else{
+                $resultStr = "对不起，绑定失败。";
+            }
+        }else{
+            $resultStr = '管理员用户已经绑定，解绑请用管理员帐户发送\'unb\'。';
+        }
+        return $resultStr;
+    }
+
+    /** 解绑微信用户 **/
+    private function unbindUser($postObj){
+        $_bindUserNo = $this->_options->bindUserNo;
+        $sendUser = (string) $postObj->FromUserName;
+        $resultStr = "";
+        if($_bindUserNo != "" && $sendUser == $_bindUserNo){
+            $array["bindUserNo"] = "";
+            $array["bindCaptcha"] = WeChatHelper_Plugin::randString(8);
+            Widget_Plugins_Edit::configPlugin("WeChatHelper", $array);
+            $resultStr = "恭喜，解除绑定成功！";
+        }
+        return $resultStr;
+    }
+
+    /***
+     * 判断用户是否为绑定用户
+     **/
+    private function isBindUser($postObj){
+        $_bindUserNo = $this->_options->bindUserNo;
+        $sendUser = (string) $postObj->FromUserName;
+        if($_bindUserNo != "" && $sendUser == $_bindUserNo){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /***
+     * 判断是否绑定了用户
+     **/
+    private function isBind(){
+        $_bindUserNo = $this->_options->bindUserNo;
+        if($_bindUserNo != null || $_bindUserNo != ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /** 清除HTML、换行、空格 **/
+    private function deleteHtml($str) { 
+        $str = trim($str); //清除字符串两边的空格
+        $str = strip_tags($str,""); //利用php自带的函数清除html格式
+        $str = preg_replace("/\t/", "", $str); //使用正则表达式匹配需要替换的内容，如：空格，换行，并将替换为空。
+        $str = preg_replace("/\r\n/", "", $str); 
+        $str = preg_replace("/\r/", "", $str); 
+        $str = preg_replace("/\n/", "", $str); 
+        $str = preg_replace("/ /", "", $str);
+        $str = preg_replace("/&nbsp; /", "", $str);  //匹配html中的空格
+        return trim($str); //返回字符串
     }
 }
 ?>
--- Plugin.php
+++ Plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * 微信助手
+ * 让你的微信公众帐号和Typecho博客联系起来
  * 
  * @package WeChatHelper
- * @author binjoo
- * @version 1.0.0
+ * @author 冰剑
+ * @version 1.1.0
  * @link http://www.binjoo.net
  */
 class WeChatHelper_Plugin implements Typecho_Plugin_Interface
@@ -20,6 +20,7 @@ class WeChatHelper_Plugin implements Typecho_Plugin_Interface
     {
         Helper::addAction('wechatHelper', 'WeChatHelper_Action');
         Helper::addRoute('wechat', '/wechat', 'WeChatHelper_Action', 'link');
+        //Helper::addPanel(1, 'WeChatHelper/Panel.php', '微信助手', '微信助手设置', 'administrator');
         return('微信助手已经成功激活，请进入设置Token!');
     }
     
@@ -35,6 +36,7 @@ class WeChatHelper_Plugin implements Typecho_Plugin_Interface
     {
         Helper::removeRoute('wechat');
         Helper::removeAction('wechatHelper');
+        //Helper::removePanel(1, 'WeChatHelper/Panel.php');
     }
     
     /**
@@ -46,18 +48,53 @@ class WeChatHelper_Plugin implements Typecho_Plugin_Interface
      */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
+
+
         /** Token **/
         $token = new Typecho_Widget_Helper_Form_Element_Text('token', NULL, '845C2550903CE6FA54CACDB82EAD4350', _t('微信Token'), '可以任意填写，用作生成签名。');
         $form->addInput($token);
+        /** 用户添加订阅欢迎语 **/
+        $welcome = new Typecho_Widget_Helper_Form_Element_Textarea('welcome', NULL, '哟，客官，您来啦！'.chr(10).'发送\'h\'让小的给您介绍一下！', '订阅欢迎语', '用户订阅之后主动发送的一条欢迎语消息。');
+        $form->addInput($welcome);
+        /** 返回最大结果条数 **/
+        $imageDefault = new Typecho_Widget_Helper_Form_Element_Text('imageDefault', NULL, 'http://s0.binjoo.net/201305/494/36081_m.png', _t('默认显示图片'), '图片链接，支持JPG、PNG格式，推荐图为80*80。');
+        $form->addInput($imageDefault);
         /** 返回最大结果条数 **/
         $imageNum = new Typecho_Widget_Helper_Form_Element_Text('imageNum', NULL, '5', _t('返回图文数量'), '图文消息数量，限制为10条以内。');
+        $imageNum->input->setAttribute('class', 'mini');
         $form->addInput($imageNum);
         /** 水墙 TOP 排行榜显示数量 **/
         $rankNum = new Typecho_Widget_Helper_Form_Element_Text('rankNum', NULL, '10', _t('访客评论排行榜'), '显示的排行榜数量。');
+        $rankNum->input->setAttribute('class', 'mini');
         $form->addInput($rankNum);
-        /** 水墙 TOP 排行榜显示数量 **/
+        /** 日志截取字数 **/
         $subMaxNum = new Typecho_Widget_Helper_Form_Element_Text('subMaxNum', NULL, '200', _t('日志截取字数'), '显示单条日志时，截取日志内容字数。');
+        $subMaxNum->input->setAttribute('class', 'mini');
         $form->addInput($subMaxNum);
+        /** 绑定校验字符串 start **/
+        $_userNo = "";
+        $_desc = "";
+        $_setKey = "";
+        $_setVal = "";
+        try {   
+            $_userNo = Helper::options()->plugin('WeChatHelper')->bindUserNo; 
+        } catch (Exception $e) {
+        } 
+        if($_userNo == null || $_userNo == ""){
+            $_desc = "【未绑定】";
+            $_setKey = "class";
+            $_setVal = "mini";
+        }else{
+            $_desc = "【<span style=\"color: #BD6800\">已绑定</span>】";
+            $_setKey = "style";
+            $_setVal = "display:none";
+        }
+        $bindCaptcha = new Typecho_Widget_Helper_Form_Element_Text('bindCaptcha', NULL, WeChatHelper_Plugin::randString(8), _t('绑定微信用户'), $_desc."激活插件时随机生成验证码，与微信用户绑定后，可以使用更多的功能。");
+        $bindCaptcha->input->setAttribute($_setKey, $_setVal);
+        $form->addInput($bindCaptcha);
+        $bindUserNo = new Typecho_Widget_Helper_Form_Element_Hidden('bindUserNo', NULL, NULL, _t('帐号绑定'), NULL);
+        $form->addInput($bindUserNo);
+        /** 绑定校验字符串 end **/
     }
     
     /**
@@ -68,4 +105,23 @@ class WeChatHelper_Plugin implements Typecho_Plugin_Interface
      * @return void
      */
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 生成随机字符串
+     *
+     * @access public
+     * @param integer $length 字符串长度
+     * @param string $specialChars 是否有特殊字符
+     * @return string
+     */
+    public function randString($length)
+    {
+        $result = "";
+        $chars = 'ACDEFGHJKMNPQRSTUVWXYZ2345679';
+        $max = strlen($chars) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $result .= $chars[rand(0, $max)];
+        }
+        return $result;
+    }
 }
